5dd6577fa3759ba3c4018ccbb7131501997f9852
张亮 <zanlan123@163.com>
2019-11-28 16:38:45 +0800

    提交主分支代码

--- gh20191128/src/App.js
+++ gh20191128/src/App.js
@@ -10,9 +10,26 @@ class App extends Component {
     return (
       <div className="App">
         <Todolist />
-        <img src="http://img3.imgtn.bdimg.com/it/u=3884173958,3732514001&fm=26&gp=0.jpg" />
+        {/* <img src="http://img3.imgtn.bdimg.com/it/u=3884173958,3732514001&fm=26&gp=0.jpg" />
         <img src={require('./assets/images/1.jpg')} />
-        <img src={imgsrc} />
+        <img src={imgsrc} /> */}
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
+        <div>2</div>
       </div>
     );
   }
