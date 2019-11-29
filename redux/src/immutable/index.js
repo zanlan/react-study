dddcb21f6e235d43eab215952096a4b8e5f52c33
张亮 <zanlan123@163.com>
2019-11-29 10:09:37 +0800

    第一次提交

--- redux/src/immutable/index.js
+++ redux/src/immutable/index.js
@@ -11,8 +11,6 @@ export default class ReactRedux extends Component{
     render(){
         return (
             <div>
-                <h2> react-redux  +   immutable  == 改造 不可变对象   </h2>
-                <hr/>
                 <ImmutableDemo></ImmutableDemo>
             </div>
         )
